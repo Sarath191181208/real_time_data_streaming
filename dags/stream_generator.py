67ce2ca229144736fe84aff3f2920d9c716ca665
Sarath <74459981+Sarath191181208@users.noreply.github.com>
2024-09-29 09:13:17 +0000

    + logging for producer

--- dags/stream_generator.py
+++ dags/stream_generator.py
@@ -4,6 +4,7 @@ from airflow.decorators import  dag
 from airflow.operators.python import PythonOperator
 from kafka import KafkaProducer
 
+import logging
 import six
 import sys
 # https://stackoverflow.com/questions/77287622/modulenotfounderror-no-module-named-kafka-vendor-six-moves-in-dockerized-djan
@@ -12,20 +13,36 @@ if sys.version_info >= (3, 12, 0):
 
 from data_generator import generate_fake_user
 
+
+logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+# logging.debug('Debugging')
+# logging.info('Information')
+# logging.warning('Warning')
+# logging.error('Error')
+# logging.critical('Critical')
+
+
 today = datetime.datetime.now()
 SECONDS = 1
 
 
 def stream_data():
+    try: 
+        producer = KafkaProducer(bootstrap_servers="broker:9092", max_block_ms=10_000*SECONDS)
+        logging.info("Connected to kafka")
+        logging.info("--"*20)
+        logging.info("Sending data ... ")
+        logging.info("--"*20)
+        for i in range(100):
+            user = generate_fake_user()
+            data = json.dumps(user, default=str).encode("utf-8")
+            logging.info(f"Sending the user data {i}")
+            logging.info(data)
+            producer.send("user_data", value=data)
+    except Exception:
+        logging.exception("Writing to kafka failed with the following error")
 
-    producer = KafkaProducer(bootstrap_servers="broker:9092", max_block_ms=10_000*SECONDS)
-    print("--"*20)
-    print("Sending data ... ")
-    print("--"*20)
-    for _ in range(100):
-        user = generate_fake_user()
-        data = json.dumps(user, default=str).encode("utf-8")
-        producer.send("user_data", value=data)
+
 
 @dag(dag_id="stream_data", start_date=datetime.datetime(2021, 1, 1), schedule_interval="@daily")
 def service_dag():
