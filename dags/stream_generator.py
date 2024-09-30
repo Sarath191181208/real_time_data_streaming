70b3e7d7132be0c6c06f49b371b43af32840d134
Sarath <74459981+Sarath191181208@users.noreply.github.com>
2024-09-30 14:29:14 +0000

    misc: formatting and others

--- dags/stream_generator.py
+++ dags/stream_generator.py
@@ -1,25 +1,26 @@
 import datetime
 import json
-from airflow.decorators import  dag
+from airflow.decorators import dag
 from airflow.operators.python import PythonOperator
 from kafka import KafkaProducer
 
 import logging
 import six
 import sys
+
 # https://stackoverflow.com/questions/77287622/modulenotfounderror-no-module-named-kafka-vendor-six-moves-in-dockerized-djan
 if sys.version_info >= (3, 12, 0):
-    sys.modules['kafka.vendor.six.moves'] = six.moves
+    sys.modules["kafka.vendor.six.moves"] = six.moves
 
 from data_generator import generate_fake_user
 
 
-logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-# logging.debug('Debugging')
-# logging.info('Information')
-# logging.warning('Warning')
-# logging.error('Error')
-# logging.critical('Critical')
+logging.basicConfig(
+    filename="log.log",
+    level=logging.DEBUG,
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
 
 
 today = datetime.datetime.now()
@@ -27,41 +28,31 @@ SECONDS = 1
 
 
 def stream_data():
-    try: 
-        producer = KafkaProducer(bootstrap_servers="broker:9092", max_block_ms=10_000*SECONDS)
+    try:
+        producer = KafkaProducer(bootstrap_servers="broker:29092")
         logging.info("Connected to kafka")
-        logging.info("--"*20)
+        logging.info("--" * 20)
         logging.info("Sending data ... ")
-        logging.info("--"*20)
-        for i in range(100):
+        logging.info("--" * 20)
+        for i in range(10):
             user = generate_fake_user()
-            data = json.dumps(user, default=str).encode("utf-8")
+            data = json.dumps(user.json()).encode("utf-8")
             logging.info(f"Sending the user data {i}")
             logging.info(data)
             producer.send("user_data", value=data)
+        producer.flush()
+        logging.info(f"{'##'*20}Sending completed{'##'*20}")
     except Exception:
         logging.exception("Writing to kafka failed with the following error")
 
 
-
-@dag(dag_id="stream_data", start_date=datetime.datetime(2021, 1, 1), schedule_interval="@daily")
+@dag(
+    dag_id="stream_data",
+    start_date=datetime.datetime(2021, 1, 1),
+    schedule_interval="@daily",
+)
 def service_dag():
-    PythonOperator(
-        task_id="streaming_task",
-        python_callable=stream_data
-    )
+    PythonOperator(task_id="streaming_task", python_callable=stream_data)
+
 
 service_dag()
-
-# import datetime
-
-# from airflow.decorators import dag
-# from airflow.operators.empty import EmptyOperator
-
-
-# @dag(dag_id="dag_id2", start_date=datetime.datetime(2021, 1, 1), schedule="@daily")
-# def generate_dag2():
-#     EmptyOperator(task_id="task2")
-
-
-# generate_dag2()
